fe05ac5ef9c86912ec5b0567d860b947757fef6e
Jeff Knapp <knappjf@cs.sunyit.edu>
2016-02-04 20:50:06 -0500

    Login in using dummy credentials, Added main menu activity

--- app/src/main/java/info/jfknapp/parkcompanion/LoginActivity.java
+++ app/src/main/java/info/jfknapp/parkcompanion/LoginActivity.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity{
+
+    private final String sDummyUserName="test";
+    private final String sDummyPassword="test";
+    private EditText usernameField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +19,15 @@ public class LoginActivity extends Activity{
     }
 
     public void login(View view){
-        Intent intent = new Intent(this, MainMenuActivity.class);
-        startActivity(intent);
+        usernameField = (EditText) findViewById(R.id.usernameField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+
+        String sUsernameInput = usernameField.getText().toString();
+        String sPasswordInput = passwordField.getText().toString();
+
+        if(sUsernameInput.equals(sDummyUserName) && sPasswordInput.equals(sDummyPassword)){
+            Intent intent = new Intent(this, MainMenuActivity.class);
+            startActivity(intent);
+        }
     }
 }
