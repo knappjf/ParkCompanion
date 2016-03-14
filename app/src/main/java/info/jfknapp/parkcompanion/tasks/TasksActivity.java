560544fa91863f3046a0ada910fe3e35c271ba72
Jeff Knapp <knappjf@cs.sunyit.edu>
2016-03-14 17:37:42 -0400

    hopefully this works

---
+++ app/src/main/java/info/jfknapp/parkcompanion/tasks/TasksActivity.java
@@ -0,0 +1,15 @@
+package info.jfknapp.parkcompanion.tasks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import info.jfknapp.parkcompanion.R;
+
+public class TasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+    }
+}
