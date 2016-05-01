ac5e4e2249d881f81f5310ae6269b1ec6eac860d
jeff <knappjf@sunyit.edu>
2016-05-01 15:34:00 -0400

    added blank park details and edit activities

---
+++ app/src/main/java/info/jfknapp/parkcompanion/parks/ParkDetailsActivity.java
@@ -0,0 +1,17 @@
+package info.jfknapp.parkcompanion.parks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import info.jfknapp.parkcompanion.R;
+
+/**
+ * Created by jeff on 5/1/16.
+ */
+public class ParkDetailsActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_park_details);
+    }
+}
--- app/src/main/java/info/jfknapp/parkcompanion/parks/ParkListActivity.java
+++ app/src/main/java/info/jfknapp/parkcompanion/parks/ParkListActivity.java
@@ -1,7 +1,9 @@
 package info.jfknapp.parkcompanion.parks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import info.jfknapp.parkcompanion.R;
 
@@ -10,5 +12,15 @@ public class ParkListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park_list);
+    }
+
+    public void onDetailsButton(View v){
+        Intent intent = new Intent(this, ParkDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onEditButton(View v){
+        Intent intent = new Intent(this, ParkEditActivity.class);
+        startActivity(intent);
     }
 }
