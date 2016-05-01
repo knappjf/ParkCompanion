2e55077acd3e2cbe917c3e068cf92fd9479e5016
Jeff <knappjf@sunyit.edu>
2016-05-01 15:18:03 -0400

    added blank contact details and edit activities

--- app/src/main/java/info/jfknapp/parkcompanion/contacts/ContactListActivity.java
+++ app/src/main/java/info/jfknapp/parkcompanion/contacts/ContactListActivity.java
@@ -1,7 +1,9 @@
 package info.jfknapp.parkcompanion.contacts;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import info.jfknapp.parkcompanion.R;
 
@@ -10,5 +12,15 @@ public class ContactListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+    }
+
+    public void onDetailsButton(View v){
+        Intent intent = new Intent(this, ContactDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onEditButton(View v){
+        Intent intent = new Intent(this, ContactEditActivity.class);
+        startActivity(intent);
     }
 }
