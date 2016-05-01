cc140a7705f2bb60594fd2329d585088f841ac95
Jeff <knappjf@sunyit.edu>
2016-05-01 15:27:09 -0400

    added blank task details and edit activities

--- app/src/main/java/info/jfknapp/parkcompanion/tasks/TaskListActivity.java
+++ app/src/main/java/info/jfknapp/parkcompanion/tasks/TaskListActivity.java
@@ -1,7 +1,9 @@
 package info.jfknapp.parkcompanion.tasks;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import info.jfknapp.parkcompanion.R;
 
@@ -15,5 +17,15 @@ public class TaskListActivity extends Activity {
         super.onCreate(savedInstanceState);
         mPresenter = new TasksPresenter(this);
         setContentView(R.layout.activity_task_list);
+    }
+
+    public void onDetailsButton(View v){
+        Intent intent = new Intent(this, TaskDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onEditButton(View v){
+        Intent intent = new Intent(this, TaskEditActivity.class);
+        startActivity(intent);
     }
 }
